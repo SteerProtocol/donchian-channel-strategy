f052a5990487177aa67e04b2732ea371f7655eb9
Brandon <brandonjmhunt@gmail.com>
2023-06-13 16:47:00 -0600

    feat: :sparkles: Tend trigger when current positions are inactive

--- assembly/index.ts
+++ assembly/index.ts
@@ -37,7 +37,31 @@ function closestDivisibleNumber(num: number, divisor: number, floor: boolean): n
   return Math.ceil(num / divisor) * divisor;
 }
 
-export function execute(_prices: string): string {
+// in this context we will return the lowest start tick and the greatest uppertick,
+//   to encompass all active areas of the current postions
+
+function parsePositions(_positions: string): Position {
+  // _positions will be '[[#,#,#],[#,#,#],[#,#,#]]' presumably. lower, upper, weight
+  // clean up our list by removing spaces and brackets
+  let positions = _positions.replaceAll(' ','');
+  positions = positions.replaceAll('[','');
+  let rangeArray = positions.split(']',2);
+  let startTick = rangeArray[0].split(',')[0];
+  const endTicks = rangeArray[1].split(',');
+  let endTick = endTicks[endTicks.length-1]
+  const strArrays = [startTick, endTick];
+  // check null
+  if (strArrays[0] == '' || strArrays[0] == null || strArrays[1] == '' || strArrays[1] == null) {
+    return new Position(0, 0, 0);
+  }
+  // else return normal
+  const lowerTick = i32(parseInt(strArrays[0]));
+  const upperTick = i32(parseInt(strArrays[1]));
+  // weights shouldn't matter in this context, we just want the total active range
+  return new Position(lowerTick, upperTick, 100);
+}
+
+export function execute(_prices: string, _positions: string, _currentTick: string): string {
   // _prices will have the results of the dc, which is only candles here
   const prices = parseCandles(_prices);
   // If we have no candles, skip action
@@ -45,7 +69,17 @@ export function execute(_prices: string): string {
     return 'continue';
   }
 
-  // Get Trailing stop price
+  let currentPosition: Position;
+  if (_currentTick == null) throw new Error("CurrentTick reading null");
+  const currentTick = parseInt(_currentTick);
+  currentPosition = parsePositions(_positions);
+
+  currentPosition = parsePositions(_positions);
+  // In bounds? skip action
+  if ((currentTick <= currentPosition.endTick && currentTick >= currentPosition.startTick)) {
+    return `continue`;
+  }
+
   const channelData = donchianChannel(prices, i32(configJson.lookback));
 
   const upperLimit: f64 = channelData[0];
@@ -119,7 +153,7 @@ export function config(): string {
   "$schema": "http://json-schema.org/draft-07/schema#",
   "title": "Strategy Config",
   "type": "object",
-  "expectedDataTypes": ["OHLC"],
+  "expectedDataTypes": ["OHLC", "ULM Positions", "Current Tick (slot0)"],
   "properties": {
     "lookback": {
       "type": "number",
